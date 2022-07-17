980d3943a10cf976880c04e0c0cdc55a0987d686
LynHJ <105850558+LynHJ@users.noreply.github.com>
2022-07-18 00:02:52 +0800

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,5 +3,64 @@ import csv
 
 input_path=os.path.join("Resources","election_data.csv")
 
-with open(input_path,"r")as csv_file:
-    csvreader=csv.reader(input_path,delimiter",")
+with open(input_path,"r",encoding='utf-8')as csv_file:
+    csvreader=csv.reader(csv_file,delimiter=",")
+    header=next(csvreader)
+
+    Charles_Casper_Stockham=[]
+    Diana_DeGette=[]
+    Raymon_Anthony_Doane=[]
+
+    for row in csvreader:
+        if str('Charles Casper Stockham')==row[2]:
+            Charles_Casper_Stockham.append(row[2])     
+        if str('Diana DeGette')==row[2]:
+            Diana_DeGette.append(row[2])    
+        if str('Raymon Anthony Doane')==row[2]:
+            Raymon_Anthony_Doane.append(row[2])
+    
+    #votes for each candidates
+    vote1=len(Charles_Casper_Stockham)
+    vote2=len(Diana_DeGette)
+    vote3=len(Raymon_Anthony_Doane)
+    
+    Total_votes=int(vote1)+int(vote2)+int(vote3)
+    
+    rate1=round((int(vote1)/Total_votes)*100,3)
+    rate2=round((int(vote2)/Total_votes)*100,3)
+    rate3=round((int(vote3)/Total_votes)*100,3)
+
+    votelist=[vote1,vote2,vote3]
+    candilist=[str('Charles Casper Stockham'),str('Diana DeGette'),str('Raymon Anthony Doane')]    
+   
+    maxvote=max(votelist)
+    P_maxvote=votelist.index(maxvote)
+    winner=candilist[P_maxvote]
+    
+    print(f'Election Results')
+    print('-'*42)
+    print(f'Total Votes:{str(Total_votes)}')
+    print('-'*42)
+    print(f'Charles Casper Stockham:{str(rate1)}% ({str(vote1)})')
+    print(f'Diana DeGette:{str(rate2)}% ({str(vote2)})')
+    print(f'Raymon Anthony Doane:{str(rate3)}% ({str(vote3)})')
+    print('-'*42)
+    print(f'Winner:{str(winner)}')
+    print('-'*42)
+
+
+output_path=os.path.join('analysis','votes.csv')
+
+with open(output_path,"w",encoding='utf-8')as csv_file:
+    csvwriter=csv.writer(csv_file,delimiter=",")
+    
+    csvwriter.writerow([f'Election Results'])
+    csvwriter.writerow(['-'*42])
+    csvwriter.writerow([f'Total Votes:{str(Total_votes)}'])
+    csvwriter.writerow(['-'*42])
+    csvwriter.writerow([f'Charles Casper Stockham:{str(rate1)}% ({str(vote1)})'])
+    csvwriter.writerow([f'Diana DeGette:{str(rate2)}% ({str(vote2)})'])
+    csvwriter.writerow([f'Raymon Anthony Doane:{str(rate3)}% ({str(vote3)})'])
+    csvwriter.writerow(['-'*42])
+    csvwriter.writerow([f'Winner:{str(winner)}'])
+    csvwriter.writerow(['-'*42])
--- Pybank/main.py
+++ Pybank/main.py
@@ -42,22 +42,22 @@ with open(input_path,"r",encoding='utf-8')as csv_file:
     
     print('Financial Analysis')
     print('-'*30)
-    print(f'Total month:{str(Total_Month)}')
+    print(f'Total Month:{str(Total_Month)}')
     print(f'Total:{str(Total)}')
-    print(f'Average change:{str(average_change)}')
+    print(f'Average Change:{str(average_change)}')
     print(f'Greatest Increase in Profits:({str(date1)}) ({str(maxnum)})')
     print(f'Greatest Decrease in Profits:({str(date2)}) ({str(minnum)})')   
 
 
 output_path=os.path.join('analysis','summary.csv')
 
-with open(output_path,"w",encoding='utf-8')as csv_file:
+with open(output_path,'w',encoding='utf-8')as csv_file:
     csvwriter=csv.writer(csv_file,delimiter=",")
     
-    csvwriter.writerow('Financial Analysis')
-    csvwriter.writerow('-'*30)
-    csvwriter.writerow(f'Total month:{str(Total_Month)}')
-    csvwriter.writerow(f'Total:{str(Total)}')
-    csvwriter.writerow(f'Average change:{str(average_change)}')
-    csvwriter.writerow(f'Greatest Increase in Profits:({str(date1)}) ({str(maxnum)})')
-    csvwriter.writerow(f'Greatest Decrease in Profits:({str(date2)}) ({str(minnum)})')
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(["-"*30])
+    csvwriter.writerow([f'Total Month:{str(Total_Month)}'])
+    csvwriter.writerow([f'Total:{str(Total)}'])
+    csvwriter.writerow([f'Average Change:{str(average_change)}'])
+    csvwriter.writerow([f'Greatest Increase in Profits:({str(date1)}) ({str(maxnum)})'])
+    csvwriter.writerow([f'Greatest Decrease in Profits:({str(date2)}) ({str(minnum)})'])
