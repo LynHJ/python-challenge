67940a31b6574de57201fddf50c8d618741f64a6
LynHJ <105850558+LynHJ@users.noreply.github.com>
2022-07-18 20:15:12 +0800

    update

--- Pybank/main.py
+++ Pybank/main.py
@@ -29,24 +29,29 @@ with open(input_path,"r",encoding='utf-8')as csv_file:
         length=len(datalist)-1
         average_change=round(change/length,2)
 
-    
-    #Max and min
-    maxnum=max(datalist)
-    minnum=min(datalist)
-    
-    P_max=datalist.index(maxnum)
-    P_min=datalist.index(minnum)
 
-    date1=timelist[P_max]
-    date2=timelist[P_min]
+    #Max and min
+    difference_list=[]    
+    for i in range(0,length):
+        difference=datalist[i+1]-datalist[i]
+        difference_list.append(difference)    
+
+    maxnum=max(difference_list)
+    minnum=min(difference_list)
+    
+    P_max=difference_list.index(maxnum)
+    P_min=difference_list.index(minnum)
+
+    date1=timelist[P_max+1]
+    date2=timelist[P_min+1]
     
     print('Financial Analysis')
     print('-'*30)
     print(f'Total Month:{str(Total_Month)}')
     print(f'Total:{str(Total)}')
     print(f'Average Change:{str(average_change)}')
-    print(f'Greatest Increase in Profits:({str(date1)}) ({str(maxnum)})')
-    print(f'Greatest Decrease in Profits:({str(date2)}) ({str(minnum)})')   
+    print(f'Greatest Increase in Profits:({str(date1)}) (${str(maxnum)})')
+    print(f'Greatest Decrease in Profits:({str(date2)}) (${str(minnum)})')   
 
 
 output_path=os.path.join('analysis','summary.csv')
@@ -59,5 +64,5 @@ with open(output_path,'w',encoding='utf-8')as csv_file:
     csvwriter.writerow([f'Total Month:{str(Total_Month)}'])
     csvwriter.writerow([f'Total:{str(Total)}'])
     csvwriter.writerow([f'Average Change:{str(average_change)}'])
-    csvwriter.writerow([f'Greatest Increase in Profits:({str(date1)}) ({str(maxnum)})'])
-    csvwriter.writerow([f'Greatest Decrease in Profits:({str(date2)}) ({str(minnum)})'])
+    csvwriter.writerow([f'Greatest Increase in Profits:({str(date1)}) (${str(maxnum)})'])
+    csvwriter.writerow([f'Greatest Decrease in Profits:({str(date2)}) (${str(minnum)})'])
